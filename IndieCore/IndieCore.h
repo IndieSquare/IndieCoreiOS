3a1a523de7f4b61d83213a6f942af3427ed0f17d
“christian <christian@cdwsydeny.com.au>
2016-10-25 11:57:24 +0900

    Initial Commit

---
+++ IndieCore/IndieCore.h
@@ -0,0 +1,13 @@
+//
+//  IndieCore.h
+//  IndieCore
+//
+//  Created by Chris on 25/10/2016.
+//  Copyright © 2016 IndieSquare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IndieCore : NSObject
+
+@end
---
+++ IndieCore/IndieCore.m
@@ -0,0 +1,13 @@
+//
+//  IndieCore.m
+//  IndieCore
+//
+//  Created by Chris on 25/10/2016.
+//  Copyright © 2016 IndieSquare. All rights reserved.
+//
+
+#import "IndieCore.h"
+
+@implementation IndieCore
+
+@end
